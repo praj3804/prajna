f76ee86eb2d7248dac491b53147782d3a675a900
praj3804 <prajjshenoy@gmail.com>
2025-06-06 01:53:26 +0530

    first commit

---
+++ src/main/java/pg4/pg4/App.java
@@ -0,0 +1,13 @@
+package pg4.pg4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
